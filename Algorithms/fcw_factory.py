ddec9c13748cba1db42f50711fe1eaa51743118c
Adam Gajdoš <adamgajdos.13@gmail.com>
2022-08-29 09:54:44 +0200

    second minor changes to naming

--- Algorithms/fcw_factory.py
+++ Algorithms/fcw_factory.py
@@ -5,7 +5,7 @@ class FCWFactory:
     def __init__(self):
         pass
 
-    def get_fcw(self, fcw_type: str) -> algo.FCWAlgorithm:
+    def get_fcw(self, fcw_type: str) -> algo.fcw_algo.FCWAlgorithm:
         if fcw_type == 'fcw_algo_ttc_braking_distance':
             return algo.fcw_algo_ttc_braking_distance.FCWAlgorithmTTCBrakingDistance()
         elif fcw_type == 'fcw_algo_ttc_1cam':
